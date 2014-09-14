9c2ea88fdb48676924688818840cbfccbb32de48
martin <martinjrb@gmail.com>
2014-09-15 00:53:14 +0100

    working dynamically with strings

--- src/AutoConfiguration.TestClient/Program.cs
+++ src/AutoConfiguration.TestClient/Program.cs
@@ -1,23 +1,42 @@
 ﻿using System;
-using System.Configuration;
 
 namespace AutoConfiguration.TestClient
 {
     public interface IConfiguration
     {
-        string Test { get; set; }
+        string TestString { get; set; }
     }
+
+    public interface IAnotherConfiguration
+    {
+        //int TestInt { get; }
+        int TestInt { get; set; }
+    }
+
+    public interface IBoolConfiguration
+    {
+        bool TestBoolT { get; }
+        bool TestBoolF { get; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             var configuration = ConfigurationReader.Read<IConfiguration>();
 
-            Console.WriteLine(configuration.Test);
-            configuration.Test = "modified";
+            Console.WriteLine(configuration.TestString);/*
+            configuration.TestString = "modified";
 
-            Console.WriteLine(configuration.Test);
+            Console.WriteLine(configuration.TestString);
 
+            var configuration2 = ConfigurationReader.Read<IAnotherConfiguration>();
+            Console.WriteLine(configuration2.TestInt);
+            */
+            /*var configuration3 = ConfigurationReader.Read<IBoolConfiguration>();
+            Console.WriteLine(configuration3.TestBoolT);
+            Console.WriteLine(configuration3.TestBoolF);
+            */
             Console.ReadKey();
         }
     }
--- src/AutoConfiguration/ConfigurationReader.cs
+++ src/AutoConfiguration/ConfigurationReader.cs
@@ -1,115 +1,200 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 
 namespace AutoConfiguration
 {
+    public class Configuration
+    {
+        /*private static readonly Dictionary<Type, Action<ILGenerator, string>> TypeConvertMappings = new Dictionary<Type, Action<ILGenerator, string>>() {
+            { typeof(bool), (generator, s) => generator.Emit(Convert.ToBoolean(s) ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0)},
+            { typeof(byte), (generator, s) => generator.Emit(OpCodes.Ldc_I4, Convert.ToByte(s))},
+            { typeof(int), (generator, s) => generator.Emit(OpCodes.Ldc_I4, Convert.().ToInt32(s))},
+            { typeof(string), (generator, s) => generator.Emit(OpCodes.Ldstr, s)}, 
+        };
+        */
+
+        internal static void SetValue(object obj, string fieldname, string value)
+        {
+            var type = obj.GetType();
+            var field = type.GetField(fieldname, BindingFlags.NonPublic | BindingFlags.Instance);
+
+            var fieldType = field.FieldType;
+
+            var convertMethod = typeof(Convert).GetMethods(BindingFlags.Public | BindingFlags.Static)
+                            .Single(m => m.Name.StartsWith("To") && m.ReturnType == fieldType && m.GetParameters().Last().ParameterType == typeof(string));
+
+            field.SetValue(obj, convertMethod.Invoke(null, new object[] { value }));
+        }
+
+        protected internal void SetValue(string fieldname, string value)
+        {
+            var type = this.GetType();
+            var field = type.GetField(fieldname, BindingFlags.NonPublic | BindingFlags.Instance);
+
+            var fieldType = field.FieldType;
+            
+            var convertMethod = typeof(Convert).GetMethods(BindingFlags.Public | BindingFlags.Static)
+                .Single(m => m.Name.StartsWith("To") && m.ReturnType == fieldType && m.GetParameters().Last().ParameterType == typeof(string));
+
+            field.SetValue(this, convertMethod.Invoke(null, new object[] { value }));
+        }
+    }
+
     public class ConfigurationReader
     {
+        private static AssemblyBuilder _assemblyBuilder;
         public static T Read<T>()
         {
-            AssemblyName aName = new AssemblyName("DynamicAutoConfiguration");
-            AssemblyBuilder ab =
-                AppDomain.CurrentDomain.DefineDynamicAssembly(
-                    aName,
-                    AssemblyBuilderAccess.RunAndSave);
+            var typeBuilder = GetTypeBuilder<T>();
 
-            // For a single-module assembly, the module name is usually 
-            // the assembly name plus an extension.
-            ModuleBuilder mb =
-                ab.DefineDynamicModule(aName.Name, aName.Name + ".dll");
-
-            TypeBuilder tb = mb.DefineType(
-            "AutoConfiguration",
-             TypeAttributes.Public, typeof(object), new[] { typeof(T) });
-
-            // Add a private field of type int (Int32).
-            FieldBuilder fbString = tb.DefineField(
-                "_test",
-                typeof(string),
-                FieldAttributes.Private);
-
-            // Define a default constructor that supplies a default value 
-            // for the private field. For parameter types, pass the empty 
-            // array of types or pass null.
-            ConstructorBuilder ctor0 = tb.DefineConstructor(
+            var constructorBuilder = typeBuilder.DefineConstructor(
                 MethodAttributes.Public,
                 CallingConventions.Standard,
                 Type.EmptyTypes);
+            
+            // The first argument to the constructor is the instance
+            var constructorIL = constructorBuilder.GetILGenerator();
+            constructorIL.Emit(OpCodes.Ldarg_0);
 
-            ILGenerator ctor0IL = ctor0.GetILGenerator();
-            // For a constructor, argument zero is a reference to the new 
-            // instance. Push it on the stack before pushing the default 
-            // value on the stack, then call constructor ctor1.
-            ctor0IL.Emit(OpCodes.Ldarg_0);
-            //ctor0IL.Emit(OpCodes.Ldc_I4_S, 42);
-            ctor0IL.Emit(OpCodes.Ldstr, "Hardcoded Test");
-            ctor0IL.Emit(OpCodes.Stfld, fbString);
-            ctor0IL.Emit(OpCodes.Ret);
 
-            PropertyBuilder pbNumber = tb.DefineProperty(
-            "Test",
-            PropertyAttributes.HasDefault,
-            typeof(string),
-            null);
 
-            // The property "set" and property "get" methods require a special
-            // set of attributes.
-            MethodAttributes getSetAttr = MethodAttributes.Public |
-                MethodAttributes.SpecialName | MethodAttributes.HideBySig;
+            var interfaceProperties = typeof (T).GetProperties();
 
-            // Define the "get" accessor method for Number. The method returns
-            // an integer and has no arguments. (Note that null could be  
-            // used instead of Types.EmptyTypes)
-            MethodBuilder mbNumberGetAccessor = tb.DefineMethod(
-                "get_Test",
-                getSetAttr | MethodAttributes.Virtual,
+            foreach (var property in interfaceProperties)
+            {
+                GenerateProperty<T>(typeBuilder, constructorIL, property);
+            }
+
+            // Return the instance
+            constructorIL.Emit(OpCodes.Ret);
+            
+            //_assemblyBuilder.Save("DynamicAutoConfiguration.dll");
+
+            var generatedType = typeBuilder.CreateType();
+
+            return (T)Activator.CreateInstance(generatedType);
+        }
+
+        /*//TODO: decimal, unsigned forms http://en.wikipedia.org/wiki/List_of_CIL_instructions
+         ;*/
+
+        static readonly MethodInfo SetValueMethod = typeof(Configuration).GetMethod("SetValue", BindingFlags.NonPublic | BindingFlags.FlattenHierarchy | BindingFlags.Instance);
+        
+        private static void GenerateProperty<T>(TypeBuilder typeBuilder, ILGenerator constructorIL, PropertyInfo property)
+        {
+            var propertyType = property.PropertyType;
+ 
+            var fieldBuilder = typeBuilder.DefineField(
+                "_" + property.Name,
+                propertyType,
+                FieldAttributes.Private);
+
+
+            
+           /* constructorIL.Emit(OpCodes.Ldarg_0);
+            constructorIL.Emit(OpCodes.Ldstr, fieldBuilder.Name);
+            constructorIL.Emit(OpCodes.Ldstr, ConfigurationManager.AppSettings[property.Name]);
+            constructorIL.EmitCall(OpCodes.Callvirt, setValueMethod, null);
+            */
+            
+
+            constructorIL.Emit(OpCodes.Ldstr, ConfigurationManager.AppSettings[property.Name]);
+            constructorIL.Emit(OpCodes.Stfld, fieldBuilder);
+
+
+            /*
+            constructorIL.Emit(OpCodes.Ldstr, fieldBuilder.Name);
+            constructorIL.Emit(OpCodes.Ldstr, ConfigurationManager.AppSettings[property.Name]);
+            constructorIL.Emit(OpCodes.Call, SetValueMethod);*/
+
+            // Tell the constructor to initialise this field with the value from AppSettings
+            //TypeStackMappings[propertyType](constructorIL, ConfigurationManager.AppSettings[property.Name]);
+            
+            /*constructorIL.Emit(OpCodes.Ldc_I4_1);
+            
+            if (propertyType == typeof(string))
+                constructorIL.Emit(OpCodes.Ldstr, ConfigurationManager.AppSettings[property.Name]);
+
+            if (propertyType == typeof(int))
+                constructorIL.Emit(OpCodes.Ldc_I4_S, Convert.ToInt32(ConfigurationManager.AppSettings[property.Name]));*/
+            
+            
+
+            var propertyBuilder = typeBuilder.DefineProperty(
+                property.Name,
+                PropertyAttributes.HasDefault,
                 typeof(string),
+                null);
+
+            const MethodAttributes getSetVirtualAttributes = MethodAttributes.Public |
+                                                             MethodAttributes.SpecialName | 
+                                                             MethodAttributes.HideBySig | 
+                                                             MethodAttributes.Virtual;
+
+            var getAccessorName = "get_" + property.Name;
+
+            var getAccessorMethodBuilder = typeBuilder.DefineMethod(
+                getAccessorName,
+                getSetVirtualAttributes,
+                propertyType,
                 Type.EmptyTypes);
 
-            tb.DefineMethodOverride(mbNumberGetAccessor, typeof(T).GetMethod("get_Test"));
+            typeBuilder.DefineMethodOverride(getAccessorMethodBuilder, typeof(T).GetMethod(getAccessorName));
+            var interfaceGetAccessor = typeof(T).GetMethod(getAccessorName);
+            if (interfaceGetAccessor != null) typeBuilder.DefineMethodOverride(getAccessorMethodBuilder, interfaceGetAccessor);
 
-            ILGenerator numberGetIL = mbNumberGetAccessor.GetILGenerator();
+            var getAccessorILGenerator = getAccessorMethodBuilder.GetILGenerator();
+
             // For an instance property, argument zero is the instance. Load the  
             // instance, then load the private field and return, leaving the 
             // field value on the stack.
-            numberGetIL.Emit(OpCodes.Ldarg_0);
-            numberGetIL.Emit(OpCodes.Ldfld, fbString);
-            numberGetIL.Emit(OpCodes.Ret);
+            getAccessorILGenerator.Emit(OpCodes.Ldarg_0);
+            getAccessorILGenerator.Emit(OpCodes.Ldfld, fieldBuilder);
+            getAccessorILGenerator.Emit(OpCodes.Ret);
 
-            pbNumber.SetGetMethod(mbNumberGetAccessor);
+            propertyBuilder.SetGetMethod(getAccessorMethodBuilder);
 
-            // Define the "set" accessor method for Number, which has no return 
-            // type and takes one argument of type int (Int32).
-            MethodBuilder mbNumberSetAccessor = tb.DefineMethod(
-                "set_Test",
-                getSetAttr | MethodAttributes.Virtual,
+            var setAccessorName = "set_" + property.Name;
+            var setAccessorMethodBuiler = typeBuilder.DefineMethod(
+                setAccessorName,
+                getSetVirtualAttributes,
                 null,
-                new Type[] { typeof(string) });
-            
-            tb.DefineMethodOverride(mbNumberSetAccessor, typeof(T).GetMethod("set_Test"));
+                new [] { propertyType });
 
-            ILGenerator numberSetIL = mbNumberSetAccessor.GetILGenerator();
-            // Load the instance and then the numeric argument, then store the 
+            var interfaceSetAccessor = typeof(T).GetMethod(setAccessorName);
+            if (interfaceSetAccessor != null) typeBuilder.DefineMethodOverride(setAccessorMethodBuiler, interfaceSetAccessor);
+
+            var setAccessorILGenerator = setAccessorMethodBuiler.GetILGenerator();
+            // Load the instance and then the argument, then store the 
             // argument in the field.
-            numberSetIL.Emit(OpCodes.Ldarg_0);
-            numberSetIL.Emit(OpCodes.Ldarg_1);
-            numberSetIL.Emit(OpCodes.Stfld, fbString);
-            numberSetIL.Emit(OpCodes.Ret);
+            setAccessorILGenerator.Emit(OpCodes.Ldarg_0);
+            setAccessorILGenerator.Emit(OpCodes.Ldarg_1);
+            setAccessorILGenerator.Emit(OpCodes.Stfld, fieldBuilder);
+            setAccessorILGenerator.Emit(OpCodes.Ret);
 
-            pbNumber.SetSetMethod(mbNumberSetAccessor);
+            propertyBuilder.SetSetMethod(setAccessorMethodBuiler);
+        }
 
+        private static TypeBuilder GetTypeBuilder<T>()
+        {
+            var assemblyName = new AssemblyName("DynamicAutoConfiguration");
+            _assemblyBuilder =
+                AppDomain.CurrentDomain.DefineDynamicAssembly(
+                    assemblyName,
+                    AssemblyBuilderAccess.RunAndSave);
 
-            //            ab.Save(aName.Name + ".dll");
+            var moduleBuilder =
+                _assemblyBuilder.DefineDynamicModule(assemblyName.Name, assemblyName.Name + ".dll");
 
-            Type t = tb.CreateType();
+            var typeBuilder = moduleBuilder.DefineType(
+            "AutoConfiguration",
+             TypeAttributes.Public, typeof(Configuration), new[] { typeof(T) });
 
-
-
-            // Create an instance of MyDynamicType using the default  
-            // constructor.  
-            T o1 = (T)Activator.CreateInstance(t);
-            return o1;
+            return typeBuilder;
         }
     }
 }
